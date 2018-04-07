fca433d627c8ba40192426892dccdf929b3d3daf
Kristofer Padilla <kristoferpadilla@yahoo.com>
2018-04-07 16:50:29 -0700

    Initial Commit

---
+++ lo-keyTests/lo_keyTests.swift
@@ -0,0 +1,36 @@
+//
+//  lo_keyTests.swift
+//  lo-keyTests
+//
+//  Created by Kristofer Padilla on 4/7/18.
+//  Copyright © 2018 Lock and Key Love Inc. All rights reserved.
+//
+
+import XCTest
+@testable import lo_key
+
+class lo_keyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
